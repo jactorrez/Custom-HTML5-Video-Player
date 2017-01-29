1d3c0d3098661114dbac43ec2c0a544edfdbac53
Javier Torrez <javierctorrez@gmail.com>
2017-01-28 21:46:50 -0500

    added scrubbing functionality

--- scripts.js
+++ scripts.js
@@ -32,12 +32,26 @@ function skip(){
 function handleRangeUpdate(){
 	video[this.name] = this.value;
 }
+
+function handleProgress(){
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+
+}
+
+function scrub(e){
+	const elWidth = progress.getBoundingClientRect().width;
+ 	const percent = (e.offsetX / elWidth);
+	video.currentTime = video.duration * percent;
+}
 /* Hook up event listeners */
 video.addEventListener('click', togglePlay);
 playBtn.addEventListener('click', togglePlay);
 
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
+video.addEventListener('timeupdate', handleProgress);
 
 skipButtons.forEach((btn) => {
 	btn.addEventListener("click", skip);
@@ -46,3 +60,9 @@ skipButtons.forEach((btn) => {
 ranges.forEach((range) => {
 	range.addEventListener("input", handleRangeUpdate);
 });
+
+let mousedown = false; 
+progress.addEventListener('click', scrub); 
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
